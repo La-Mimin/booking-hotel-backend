25d14b8469e6b0b9ed7e3aeb2490077da95b48ed
ikrom130 <amqikrom@gmail.com>
2025-11-30 11:02:50 +0900

    make tset function for Room

---
+++ tests/Feature/RoomTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RoomTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    // public function test_example(): void
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
+    public function test_room_crud() {
+        // 1. Register User
+        $register = $this->postJson('api/register', [
+            'name' => 'Test User',
+            'email' => 'test@gmail.com',
+            'password' => 'password',
+        ]);
+
+        $register->assertStatus(201);
+
+        // 2. Login User
+        $login = $this->postJson('api/login', [
+            'email' => 'test@gmail.com',
+            'password' => 'password',
+        ]);
+
+        $login->assertStatus(200);
+        $token = $login->json('token');
+
+        // 3. Create room
+        $create = $this->postJson('/api/rooms', [
+            'name' => 'Deluxe Room 3',
+            'description' => 'Kamar bagus',
+            'price' => 500000,
+            'stock' => 5,
+        ], [
+            'Authorization' => "Bearer $token"
+        ]);
+
+        $create->assertStatus(201);
+        $roomId = $create->json('id');
+
+        // 4. Read room (GET)
+        $read = $this->getJson("/api/rooms/$roomId", [
+            'Authorization' => "Bearer $token"
+        ]);
+
+        $read->assertStatus(200);
+
+        // 5. Update room
+        $update = $this->putJson("/api/rooms/$roomId", [
+            'name' => 'Update room',
+            'price' => 900000,
+            'stock' => 3,
+        ], [
+            'Authorization' => "Bearer $token"
+        ]);
+
+        $update->assertStatus(200);
+        $this->assertEquals('Update room', $update->json('name'));
+
+        // 6. Delete room
+        $delete = $this->deleteJson("/api/rooms/$roomId", [], [
+            'Authorization' => "Bearer $token"
+        ]);
+
+        $delete->assertStatus(200);
+        $this->assertEquals('Room deleted', $delete->json('message'));
+    }
+
+}
